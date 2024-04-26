1f434644d0549d36f9d9577d5ee2d5f083d9770c
chjm6 <chjm655@gmail.com>
2024-04-26 18:27:42 -0400

    Keep some items in Cyclic

--- Better MC [FORGE] 1.20.1 v26/kubejs/server_scripts/cyclic.js
+++ Better MC [FORGE] 1.20.1 v26/kubejs/server_scripts/cyclic.js
@@ -1,7 +1,7 @@
 ServerEvents.recipes(event => {
 	// Generate a list of all armor and tool recipes
 	const materials = ['amethyst', 'copper', 'emerald', 'sandstone', 'netherbrick'];
-	const types = ['helmet', 'chestplate', 'leggings', 'boots', 'pickaxe', 'sword', 'axe', 'hoe', 'shovel', 'sword'];
+	const types = ['helmet', 'chestplate', 'leggings', 'boots', 'pickaxe', 'sword', 'axe', 'hoe', 'shovel', 'sword', 'carrot', 'apple', ];
 	const outputs = [];
 	for (const material of materials) {
 		for (const type of types) {
@@ -10,7 +10,38 @@ ServerEvents.recipes(event => {
 	}
 
 	outputs.push(
-		{ output: 'apotheosis:potion_charm' }
+		{ output: 'apotheosis:potion_charm' },
+		{ output: 'cyclic:soulstone' },
+		{ output: 'cyclic:evoker_fang' },
+		{ output: 'cyclic:ender_eye_reuse' },
+		{ output: 'cyclic:ender_pearl_reuse' },
+		{ output: 'cyclic:ender_pearl_mounted' },
+		{ output: 'cyclic:boomerang_carry' },
+		{ output: 'cyclic:boomerang_damage' },
+		{ output: 'cyclic:boomerang_stun' },
+		{ output: 'cyclic:heart' },
+		{ output: 'cyclic:netherite_nugget' },
+		{ output: 'cyclic:fireball' },
+		{ output: 'cyclic:netherite_nugget' },
+		{ output: 'cyclic:glove_climb' },
+		
+		// Scepters
+		{ output: 'cyclic:ice_scepter' },
+		{ output: 'cyclic:fire_scepter' },
+		{ output: 'cyclic:lightning_scepter' },
+
+		// Charms
+		{ output: 'cyclic:charm_fire' },
+		{ output: 'cyclic:antigravity' },
+		{ output: 'cyclic:charm_wither' },
+		{ output: 'cyclic:charm_void' },
+		{ output: 'cyclic:charm_antidote' },
+
+		// Quivers
+		{ output: 'cyclic:quiver_damage' },
+		{ output: 'cyclic:quiver_lightning' },
+		
+		{ output: 'cyclic:gem_obsidian' }
 	);
 
 	console.info(outputs);
@@ -19,4 +50,23 @@ ServerEvents.recipes(event => {
 		mod: 'cyclic',
 		not: outputs
 	});
+
+	// Alternate recipes
+	event.shapeless("cyclic:gem_amber",
+		[
+			'minecraft:magma_block',
+			'minecraft:fire_charge',
+			'minecraft:redstone',
+			'minecraft:honey_bottle'
+		]
+	);
+
+	event.shapeless("cyclic:gem_obsidian",
+		[
+			'cyclic:gem_amber',
+			'minecraft:obsidian',
+			'minecraft:wither_rose',
+			'minecraft:slime_ball'
+		]
+	);
 });
